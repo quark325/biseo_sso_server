002ce36e2a1ff461291caa2c8699ec316fa8d66d
Byungseo <quark325@gmail.com>
2020-04-25 12:48:35 +0000

    Add callback default function

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const app = express()
 const RedisStore = connectRedis(session)
 const redisClient = new Redis(3001)
 
+app.use(express.json())
+app.use(express.static('public'))
+
 app.use(session({
 	resave: false,
 	saveUninitialized: false,
@@ -19,7 +22,31 @@ app.use(session({
 	cookie: {maxAge: 60000},
 	}))
 
-app.listen(3000, () => {
+app.set('port', 3000)
+app.set('jwt-secret', process.env.JWT_SECRET)
+
+app.get('/', (req, res) => {
+	res.sendFile('index.html')
+})
+
+app.post('/login', (req, res) => {
+	const { url, state } = client.getLoginParams();
+	req.session.state = state; // state 값을 session에 저장합니다.
+	res.redirect(url); // 사용자를 loginUrl로 redirect 시킵니다.
+})
+
+app.get('/account/login/callback', async (req, res) => {
+	const {code, state} = req.query
+	const stateBefore = req.session.state 
+	if (stateBefore !== state) {
+		throw new Error('TOKEN MISMATCH: session might be hijacked!');
+	}
+
+	const userData = await client.getUserInfo(code); 
+	res.json(userData)	
+})
+
+app.listen(app.get('port'), () => {
 	console.log('Sever started on port 3000');
 })
 
